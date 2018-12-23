888a35adf917328312d3040ebd338f7b8fcb31aa
azizfaozi <admin@labseni.com>
2018-12-23 16:21:11 +0700

    KAU sayang AKU

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<!-- ./php/index.php -->
+<!DOCTYPE html>
+  <head>
+    <title>Assek dengan Docker</title>
+  </head>
+  
+  <body>
+    <p>Ini baru menarik sih.</p>
+  </body>
+
+</html>
+
+<?php
+  echo "assek";
+?>
